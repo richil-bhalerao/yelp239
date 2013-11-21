d5305255bd972bc3638e436d3135abddb96d5013
virajh <virajhardikar@yahoo.co.in>
2013-11-21 03:24:43 -0800

    Frontend added.

--- mysite/mysite/users/admin.py
+++ mysite/mysite/users/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
 
-from models import moocsList
 
-
-
-admin.site.register(moocsList)
--- mysite/mysite/users/models.py
+++ mysite/mysite/users/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 
-class moocsList(models.Model):
-    team = models.CharField(max_length=30)
-    path=models.CharField(max_length=100)
-    
-class ipaddress(models.Model):
-    ip=models.CharField(max_length=30)
